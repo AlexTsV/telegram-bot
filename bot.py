2bf6d0fb5f3f3f025d7b540b28de1c4670c54717
AlexTsV <34660826+AlexTsV@users.noreply.github.com>
2019-04-26 18:46:43 +0300

    ok

--- bot.py
+++ bot.py
@@ -1,70 +1,34 @@
-import requests
-import datetime
+from telegram import Bot
+from telegram import Update
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+from config import TG_TOKEN
 
-class BotHandler:
+def start(bot: Bot, update: Update):
+    bot.send_message(chat_id=update.message.chat_id, text='Privet')
 
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-    def get_updates(self, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
+def echo(bot: Bot, update: Update):
+    text = update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=text)
 
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-greet_bot = BotHandler('758306079:AAEAL86jzh6_eowV8Ay6gTQ2cLUmNIrbujk')
-greetings = ('hello', 'hi', 'greetings', 'sup')
-now = datetime.datetime.now()
 
 
 def main():
-    new_offset = None
-    today = now.day
-    hour = now.hour
+    bot = Bot(token=TG_TOKEN)
+    updater = Updater(bot=bot)
 
-    while True:
-        greet_bot.get_updates(new_offset)
+    start_handler = CommandHandler('start', start)
+    messege_handler = MessageHandler(Filters.text, echo)
 
-        last_update = greet_bot.get_last_update()
+    updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(messege_handler)
 
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+    updater.start_polling()
+    updater.idle()
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
-            today += 1
-
-        new_offset = last_update_id + 1
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+    main()

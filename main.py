803f48a0c2739d4b0f3dead7f66a3a19947d3c8c
AlexTsV <34660826+AlexTsV@users.noreply.github.com>
2019-05-05 17:59:16 +0300

    ok

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
-from telegram import Bot
-from telegram import Update
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegram.ext import Filters
-TG_TOKEN = '758306079:AAEAL86jzh6_eowV8Ay6gTQ2cLUmNIrbujk'
+TOKEN = '758306079:AAEAL86jzh6_eowV8Ay6gTQ2cLUmNIrbujk'
+import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def start(bot: Bot, update: Update):
-    bot.send_message(chat_id=update.message.chat_id, text='Я в разработке')
+def start(bot, update):
+    keyboard = [[InlineKeyboardButton("FAQ", callback_data='1'),
+                 InlineKeyboardButton("Полезные материалы", callback_data='2')],
+                [InlineKeyboardButton("Телефонный справочник МСР МО", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Выбери раздел:', reply_markup=reply_markup)
 
 
-# def echo(bot: Bot, update: Update):
-#     chat_id = update.message.chat_id
-#     chat_name = update.message.chat.first_name
-#     text = f'Привет, {chat_name}'
-#     bot.send_message(chat_id=chat_id, text=text)
+def button(bot, update):
+    query = update.callback_query
+    if query.data == '1':
+        bot.edit_message_text(text="FAQ",
+                              chat_id=query.message.chat_id,
+                              message_id=query.message.message_id)
+    if query.data == '2':
+        bot.edit_message_text(text="Links",
+                              chat_id=query.message.chat_id,
+                              message_id=query.message.message_id)
+    if query.data == '3':
+        bot.edit_message_text(text="Введи фамилию для поиска",
+                              chat_id=query.message.chat_id,
+                              message_id=query.message.message_id)
+
+
+
+
+
+def help(bot, update):
+    update.message.reply_text("Используй /start для работы с ботом.")
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def main():
-    bot = Bot(token=TG_TOKEN)
-    updater = Updater(bot=bot)
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(TOKEN)
 
-    start_handler = CommandHandler('start', start)
-    # messege_handler = MessageHandler(Filters.text, echo)
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_error_handler(error)
 
-    updater.dispatcher.add_handler(start_handler)
-    # updater.dispatcher.add_handler(messege_handler)
-
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
     updater.idle()
 
 

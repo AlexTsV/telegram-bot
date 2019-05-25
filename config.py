d3e18a664da10adef98a15f1e8ffdc29451e56be
AlexTsV <alex944480@gmail.com>
2019-05-25 11:58:40 +0300

    initial_commit

--- config.py
+++ config.py
@@ -5,3 +5,18 @@ TG_API_ID = '780919'
 TG_API_HASH = '5e4ebadc59ba54fc060dd575a0775b92'
 
 CHAT_ID = 'https://t.me/joinchat/CmGDh0PVJZRdJjuqcK5C8A'
+
+
+DB_HOST = 'ec2-50-17-246-114.compute-1.amazonaws.com'
+
+DB_NAME = 'd1himk5nf6qlu1'
+
+USER = 'hkrjksrywutriz'
+
+PASS = '6dSf3&`YIk\7LmQ"_zBb'
+
+URL = 'postgres://hkrjksrywutriz:6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e@ec2-50-17-246-114.compute-1.amazonaws.com:5432/d1himk5nf6qlu1'
+
+
+
+
--- db_app.py
+++ db_app.py
@@ -6,6 +6,8 @@ from telegram.ext import ConversationHandler
 
 import tg_api
 
+import config
+
 
 class Postgres:
     FINISH_FAQ_TO_DB, FINISH_MATERIALS_TO_DB, INSERT_FAQ_TO_DB, INSERT_MATERIALS_TO_DB, DELETE_MATERIALS, DELETE_FAQ, \
@@ -23,7 +25,7 @@ class Postgres:
         user_id = update.message.from_user['id']
         participant_list = tg_api.participants['users']
         if user_id in participant_list:
-            with psycopg2.connect("dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com") as conn:
+            with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
                 with conn.cursor() as cur:
                     cur.execute("""SELECT problem, decision FROM faq""")
                     res = cur.fetchall()
@@ -43,7 +45,7 @@ class Postgres:
         user_id = update.message.from_user['id']
         participant_list = tg_api.participants['users']
         if user_id in participant_list:
-            with psycopg2.connect("dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com") as conn:
+            with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
                 with conn.cursor() as cur:
                     cur.execute("""SELECT description, url FROM materials""")
                     res = cur.fetchall()
@@ -61,7 +63,7 @@ class Postgres:
     @staticmethod
     def received_contact(bot, update, user_data):
         text = update.message.text
-        with psycopg2.connect("dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com") as conn:
+        with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
             with conn.cursor() as cur:
                 cur.execute("""SELECT Подразделение, Должность, ФИО, Телефон, Вн, Почта FROM phonebook 
                                WHERE ФИО ILIKE '%%%s%%'""" % (text,), )
@@ -83,7 +85,7 @@ class Postgres:
     def insert_faq_to_db(bot, update, user_data):
         text = update.message.text
         user_data['decision'] = text
-        with psycopg2.connect("dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com") as conn:
+        with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
             with conn.cursor() as cur:
                 cur.execute("""INSERT INTO faq (problem, decision) VALUES (%s, %s) RETURNING problem, decision""",
                             (user_data['problem'],
@@ -99,7 +101,7 @@ class Postgres:
     def insert_materials_to_db(bot, update, user_data):
         text = update.message.text
         user_data['url'] = text
-        with psycopg2.connect("dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com") as conn:
+        with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
             with conn.cursor() as cur:
                 cur.execute("""INSERT INTO materials (description, url) VALUES (%s, %s) RETURNING description, url""",
                             (user_data['description'],
@@ -115,7 +117,7 @@ class Postgres:
     def delete_faq_from_db_1(bot, update, user_data):
         text = update.message.text
         user_data['choice'] = text
-        with psycopg2.connect("dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com") as conn:
+        with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
             with conn.cursor() as cur:
                 cur.execute("""DELETE FROM faq WHERE problem = %s RETURNING problem""",
                             (user_data['choice'],))
@@ -134,7 +136,7 @@ class Postgres:
     def delete_materials_from_db(bot, update, user_data):
         text = update.message.text
         user_data['choice'] = text
-        with psycopg2.connect("dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com") as conn:
+        with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
             with conn.cursor() as cur:
                 cur.execute("""DELETE FROM materials WHERE description = %s RETURNING description""",
                             (user_data['choice'],))
@@ -166,7 +168,7 @@ class Postgres:
                 str = ''.join(row)
                 cur_arr = str.split(';')
                 contacts_arr.extend([cur_arr])
-        with psycopg2.connect('dbname=d1himk5nf6qlu1 user=hkrjksrywutriz password=6cd6f018b5e4090b98716f2ff239438e9ceb3b25d5294410e006cea5cfa4284e host=ec2-50-17-246-114.compute-1.amazonaws.com') as conn:
+        with psycopg2.connect(f"dbname=telebot user=postgres password={config.PASS}") as conn:
             with conn.cursor() as cur:
                 cur.execute("""TRUNCATE TABLE phonebook RESTART IDENTITY""")
                 count = 0

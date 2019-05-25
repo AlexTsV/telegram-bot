a0719e88b144dd493f17db4febf57bd5153e246e
AlexTsV <34660826+AlexTsV@users.noreply.github.com>
2019-05-25 11:23:02 +0300

    initial_commit

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = '758306079:AAEAL86jzh6_eowV8Ay6gTQ2cLUmNIrbujk'
+
+TG_API_ID = 780919
+
+TG_API_HASH = '5e4ebadc59ba54fc060dd575a0775b92'
+
+CHAT_ID = 'https://t.me/joinchat/E5vPV0PVJZTSiPhGUCKVLw'
+
+# CHAT_ID = "-1001138042260"
+
+DB_PASS = '6dSf3&`YIk\7LmQ"_zBb'
